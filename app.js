1a94d96610d189eb45b824cb67f2d16d6655c601
SSrivastava18 <saurabh18srivastava1@gamil.com>
2024-11-19 18:02:46 +0530

    practice

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+// app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get('/', (req,res) =>{
+    res.send("hii");
+});
+
+
+app.listen(3000);
