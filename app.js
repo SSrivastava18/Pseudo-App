ff56d0375fca067358d8809a6cfaa7b9bd72c59b
SSrivastava18 <saurabh18srivastava1@gamil.com>
2024-11-19 18:19:02 +0530

    index

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 
 
-// app.set("view engine","ejs");
+app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 // app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get('/', (req,res) =>{
-    res.send("hii");
+    res.render("index");
 });
+
 
 
 app.listen(3000);
