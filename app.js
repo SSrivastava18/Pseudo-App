0f3f3e5ac08aa2f895f1ad86afc817d9f3bd140d
SSrivastava18 <saurabh18srivastava1@gamil.com>
2024-11-19 19:07:15 +0530

    registered

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const userModel = require("./Models/user");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 app.set("view engine","ejs");
@@ -10,6 +13,29 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req,res) =>{
     res.render("index");
+});
+
+app.post('/register', async (req,res)=>{
+    let {email, password, username, name, age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send("user already registered");
+    
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async(err, hash) =>{
+            let user = await userModel.create({
+                
+                    username,
+                    email,
+                    age,
+                    name,
+                    password: hash
+            });
+            let token = jwt.sign({email:email, userid: user._id}, "shhhh");
+            res.cookie("token", token);
+            res.send("registered");
+        })
+    })
 });
 
 
